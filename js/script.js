82a574917b8c9950784576acfeee2c748636c8ca
rafal A <rafalandrzejczuk1@gmail.com>
2022-12-25 21:43:17 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+console.log('ustaw tlo elementow ');
+
+const setBackground = () => {
+let p1 = document.querySelector('.first')
+let p2 = document.querySelector('.last')
+
+// p1.style.backgroundColor = 'red';
+// p2.style.backgroundColor = 'yellow';
+1``
+p1.classList.toggle('bg-red');
+p2.classList.toggle('bg-yellow');
+
+// toggle sprawdza czy klasa jest jak nie to dodaje i na odwrot
+
+
+
+
+
+console.log('click :)' );
+}
+
+let btnSetBackground = document.getElementById('set-background');
+
+btnSetBackground.addEventListener('click',setBackground);
+
