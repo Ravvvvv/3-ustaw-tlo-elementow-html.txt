422cbfb955fdaad41830693f238694691837e8a6
rafal A <rafalandrzejczuk1@gmail.com>
2022-12-29 16:57:22 +0100

    5 commit

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ let p2 = document.querySelector('.last')
 
 // p1.style.backgroundColor = 'red';
 // p2.style.backgroundColor = 'yellow';
-1``
+
 p1.classList.toggle('bg-red');
 p2.classList.toggle('bg-yellow');
 
